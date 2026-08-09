dc5acfb77659c8f29b03bad0d883feeb4d61164e
FedorMilovanov <258225528+FedorMilovanov@users.noreply.github.com>
2026-08-10 00:03:03 +0300

    Keep Canon technique index inside main landmark

--- src/components/CanonPageShell.tsx
+++ src/components/CanonPageShell.tsx
@@ -2,7 +2,6 @@ import { lazy, Suspense, useCallback, useEffect, useRef, useState } from 'react'
 import Header from './Header'
 import Footer from './Footer'
 import CanonExperience from './CanonExperience'
-import CanonTechniqueMatrix from './CanonTechniqueMatrix'
 import Cursor from './Cursor'
 import ScrollProgress from './ScrollProgress'
 import ScrollToTop from './ScrollToTop'
@@ -72,7 +71,6 @@ export default function CanonPageShell({ articles }: { articles: ArticleClientMe
         />
 
         <CanonExperience />
-        <CanonTechniqueMatrix />
         <Footer />
 
         {commandOpen && (
